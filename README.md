019cfb1a0c030ab0d4dd2afca44a6d8a35f7b298
danebc <carlosvidez08@gmail.com>
2023-08-06 22:44:03 -0600

    Parte 1 Proyecto integrador parte 1

---
+++ README.md
@@ -0,0 +1,1 @@
+Hola mi nombre es Carlos Videz, este es mi proyecto integrador acerca del juego El Laberinto.
---
+++ main.py
@@ -0,0 +1,9 @@
+def Laberinto(name:str): 
+    return name
+ 
+print ("Hola, bienvenido al Laberinto, por favor ingresa tu nombre") 
+if Laberinto: 
+        name = Laberinto (name=input())
+        texto = "Hola," 
+        print (texto,Laberinto(name))
+        
