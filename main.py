cf39947b1952b8a64a63bbde3403a685c9c11f62
danebc <carlosvidez08@gmail.com>
2023-08-09 23:23:31 -0600

    parte 2

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ if Laberinto:
         texto = "Hola," 
         print (texto,Laberinto(name))
         
+import keyboard
+
+print("Presiona la tecla '1' para detener el bucle.")
+
+while True:
+    event = keyboard.read_event()
+    if event.name == '1' and event.event_type == keyboard.KEY_UP:
+        print("Tecla '1' detectada. Saliendo del bucle.")
+        break
+    else:
+        print("Tecla detectada:",event.name) 
+        
+        
